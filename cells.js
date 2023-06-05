55e2bf02df1513f5f3f23feaba15ed21568fd054
Kristian Houghtling <32035589+kristian-houghtling@users.noreply.github.com>
2023-06-05 13:07:59 -0500

    init

---
+++ cells.js
@@ -0,0 +1,95 @@
+class Cells {
+    constructor(i, j, a) {
+        this.i = i; //i coordinate
+        this.j = j; //j coordinate
+        this.walls = [true, true, true, true]; //checks if there should be a wall or not
+        this.visited = false; //boolean for keeping track of whether a cell has been visited or not
+        this.a = a; //array to keep track of the cells
+    }
+
+    show() {
+        let x = this.i * w;
+        let y = this.j * w;
+        let alpha = this.a;
+
+        noFill();
+        strokeWeight(0);
+        stroke(255);
+
+        if (this.walls[0]) {
+            line(x, y, x + w, y); //top line of cell
+        }
+        if (this.walls[1]) {
+            line(x + w, y, x + w, y + w); //line on the right of cell
+        }
+        if (this.walls[2]) {
+            line(x + w, y + w, x, y + w); //bottom line of cell
+        }
+        if (this.walls[3]) {
+            line(x, y + w, x, y); //line on the left of cell
+        }
+        if (this.visited) { //colors the current visited cell
+            noStroke();
+            fill(255, 0, 255, alpha);
+            rect(x, y, w, w);
+            fill(255);
+            text(alpha, x + (w / 2), y + (w / 2));
+        }
+    }
+
+    showText() {
+        let x = this.i * w;
+        let y = this.j * w;
+        // black text
+        stroke(0);
+        fill(0);
+        textAlign(CENTER, CENTER);
+        textSize(w / 2);
+        // draw letter in center of cell
+        text(alpha, x + (w / 2), y + (w / 2));
+    }
+
+    highlight() {
+        let x = this.i * w;
+        let y = this.j * w;
+        stroke(255);
+        fill(0, 255, 0);
+        rect(x, y, w, w);
+    }
+
+    showStuck() {
+        let x = this.i * w;
+        let y = this.j * w;
+        stroke(255);
+        fill(255, 0, 0);
+        rect(x, y, w, w);
+    }
+
+    checkNeighbors() {
+        let neighbors = [];
+
+        let top = grid[index(this.i, this.j - 1)];
+        let right = grid[index(this.i + 1, this.j)];
+        let bottom = grid[index(this.i, this.j + 1)];
+        let left = grid[index(this.i - 1, this.j)];
+
+        if (top && !top.visited) {
+            neighbors.push(top);
+        }
+        if (right && !right.visited) {
+            neighbors.push(right);
+        }
+        if (bottom && !bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbors.push(left);
+        }
+        if (neighbors.length > 0) {
+            var r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        } else {
+            return undefined;
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,96 @@
+let cols;
+let rows;
+let w = 30; //gridsize
+let grid = [];
+let current; //this will hold the current(active) cell
+let stack = [];
+let preAlpha = 25;
+let alpha;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cols = floor(width / w);
+  rows = floor(height / w);
+
+  frameRate(30);
+
+  //creates the cell objects and pushes them into the grid array
+  for (j = 0; j < rows; j++) {
+    for (i = 0; i < cols; i++) {
+      let cell = new Cells(i, j);
+      grid.push(cell);
+    }
+  }
+  //initiation of the first, active cell  
+  current = random(grid);
+  current.a = preAlpha;
+
+}
+
+function draw() {
+  background(0);
+
+
+  //loops through all the objects (each cell) and call the .show function on each cell in the grid array
+  for (i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+
+  current.visited = true;
+  current.highlight();
+  current.showText();
+
+  var next = current.checkNeighbors();
+
+  if (next) {
+    next.visited = true;
+    preAlpha++;
+    alpha = floor(map(preAlpha, 25, cols * rows, 25, 225));
+    next.a = alpha;
+
+    stack.push(current);
+    removeWalls(current, next);
+    current = next;
+
+  } else if (stack.length > 0) {
+    current.showStuck();
+    current = stack.pop();
+  }
+
+}
+
+function index(i, j) {
+  //fancy way to fudge a two dimensional array without one
+  //the if statement is checking to see if any neighbors to the current cell are invalid
+  //(an edge case..meaning outside of the grid)
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+  return i + j * cols;
+}
+
+function removeWalls(a, b) {
+  let x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  let y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
+
+
+
+
+
+
